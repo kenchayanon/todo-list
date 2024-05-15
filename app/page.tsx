a67e4f737cf19364dadaeafd1872e5561471f536
kenchayanon <72121323+kenchayanon@users.noreply.github.com>
2024-05-15 16:49:40 +0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,139 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+import React, { useState, ChangeEvent, useEffect } from 'react';
+import './styles.css'; // import styles.css
+interface ListItem {
+  text: string;
+  isChecked: boolean;
 }
+
+const LeftAppBar: React.FC = () => {
+  const [listItems, setListItems] = useState<ListItem[]>([]);
+  const [inputValue, setInputValue] = useState<string>('');
+  const [checkAll, setCheckAll] = useState<Boolean>(true);
+  useEffect(() => {
+    // เมื่อ component ถูก mount ให้ดึงข้อมูลจาก API
+    fetch('https://dummyjson.com/todos')
+      .then(response => response.json())
+      .then(data => {
+        // เมื่อข้อมูลถูกโหลดสำเร็จ ให้ตั้งค่า state
+        const l:ListItem[] = [];
+        for (let i = 0; i < data.todos.length; i++) {
+          l.push({ text: data.todos[i].todo, isChecked: false });
+        }
+        // data.todo.map((item: any) => {
+        //   l.push({ text: item.title, isChecked: false });
+        // })
+        
+        setListItems(l);
+      })
+      .catch(error => {
+        // หากเกิดข้อผิดพลาดในการโหลดข้อมูล
+        console.error('Error fetching data:', error);
+      });
+  }, []); 
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleAddToList = () => {
+    if (inputValue.trim() !== '') {
+      setListItems([...listItems, { text: inputValue, isChecked: false }]);
+      setInputValue('');
+    }
+  };
+
+  const handleCheckboxChange = (index: number) => {
+    const newList = [...listItems];
+    newList[index].isChecked = !newList[index].isChecked;
+    setListItems(newList);
+  };
+
+  const handleCheckboxChangeAll = () => {
+    const newList = [...listItems];
+    for(let i = 0; i < newList.length; i++) {
+      if (checkAll)
+        newList[i].isChecked = true;
+      else
+        newList[i].isChecked = false;
+    }
+
+    setCheckAll(!checkAll);
+    setListItems(newList);
+  };
+
+  const handleDeleteItem = (index: number) => {
+    const newList = [...listItems];
+    newList.splice(index, 1);
+    setListItems(newList);
+  };
+
+  const countCheckedItems = () => {
+    return listItems.filter(item => item.isChecked).length;
+  };
+
+  const countAllItems = () => {
+    return listItems.length;
+  };
+  const getPercentage = () => {
+    const a = countCheckedItems();
+    const b = countAllItems();
+    if (a && b) {
+    return ((countCheckedItems() / countAllItems()) * 100).toFixed(2) + '%';
+    }else {
+      return '0%';
+    }
+  }
+
+
+  return (
+    
+    <div className="container">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"></link>
+
+      <h1>Todo List</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          id="fname"
+          name="fname"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Enter a new todo..."
+        />
+        <button className='button1' onClick={handleAddToList}>Add Todo</button>
+      </div>
+      <div className="card">
+        <h2 className='title'>Default Category <button className='button2' onClick={handleCheckboxChangeAll}>select all</button></h2>
+        <div className="w3-light-grey">
+  <div id="myBar" className="w3-container w3-green w3-center" style={{width: getPercentage(), marginBottom: "20px", borderRadius: "10px"}}>{getPercentage()}</div>
+</div>
+        <ul>
+          
+            {listItems.map((item, index) => (
+              <li key={index}>
+                <input
+                  type="checkbox"
+                  checked={item.isChecked}
+                  onChange={() => handleCheckboxChange(index)}
+                />
+                <p className={item.isChecked ? 'checked' : ''}>{item.text}</p>
+                <button type="button" className="deletebtn" onClick={() => handleDeleteItem(index)}>X</button>
+              </li>
+            ))}
+        </ul>
+      </div>
+      <div className="bar">
+        <p>Checked items: {countCheckedItems()} / {countAllItems()}</p>
+      </div>
+
+ 
+    </div>
+  );
+};
+
+export default LeftAppBar;
+
